9175f3906ec7fcc1b38c07f63840fcfe736daaa5
Lukasz Nitkowski <lukasz.nitkowski@pl.pwc.com>
2020-12-30 11:52:32 +0100

    leafleat

--- magia-ogrodow/src/components/contact/contact.js
+++ magia-ogrodow/src/components/contact/contact.js
@@ -1,9 +1,10 @@
 import React, { memo } from "react";
 import Parallax from '../parallax';
 import imagePlantNurseryParallax from '../../images/contact.jpg';
-import styled, {keyframes} from "styled-components";
+import styled, { keyframes } from "styled-components";
 import IconButton from "@material-ui/core/IconButton";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const ContactBlockInfo = styled.div`
   text-align: center;
@@ -32,47 +33,61 @@ const StyledKeyboardArrowDownIcon = styled(KeyboardArrowDownIcon)`
 `;
 
 const BlockWithMap = styled.div`
-
+  padding-bottom: 80px;
+  padding-top: 80px;
 `;
 
 const Contact = () => {
 
-    const scrollDown = (e) => {
-      let pageHeight = window.innerHeight;
-      window.scrollBy(0, pageHeight);
-    };
-
-    return (
-      <div>
-        <Parallax image={imagePlantNurseryParallax} />
-        <ContactBlockInfo>
-          <ContactSubBlockInfo>
-            <h2>NAPISZ DO NAS</h2>
-            <p>Jeśli masz pytania to napisz do nas, a napewno Ci odpowiemy! Dane kontaktowe znajdują się u dołu strony.</p>
-          </ContactSubBlockInfo>
-        </ContactBlockInfo>
-        <ContactBlockInfo>
-          <ContactSubBlockInfo>
-            <h2>DOJAZD</h2>
-            <p>Szkółka zlokalizowana jest na granicy Rumi oraz Dębogórze Wybudowanie. Istnieje możliwość dojazdu autobusem z dworca PKP Rumia nr 86. Szczegółowe informacje jak numer telefonu, adres można znaleźć na dole strony.
-          </p>
-            <StyledIconButton
-              color="primary"
-              aria-label="upload picture"
-              component="span"
-              onClick={scrollDown}
-            >
-              <StyledKeyboardArrowDownIcon
-                style={{ width: "100px", height: "100px" }}
-              />
-            </StyledIconButton>
-          </ContactSubBlockInfo>
-          <BlockWithMap>
-
-          </BlockWithMap>
-        </ContactBlockInfo>
-      </div>
-    );
+  const scrollDown = (e) => {
+    let pageHeight = window.innerHeight;
+    window.scrollBy(0, pageHeight);
   };
 
-  export default memo(Contact);
+  return (
+    <div>
+      <Parallax image={imagePlantNurseryParallax} />
+      <ContactBlockInfo>
+        <ContactSubBlockInfo>
+          <h2>NAPISZ DO NAS</h2>
+          <p>Jeśli masz pytania to napisz do nas, a napewno Ci odpowiemy! Dane kontaktowe znajdują się u dołu strony.</p>
+        </ContactSubBlockInfo>
+      </ContactBlockInfo>
+      <ContactBlockInfo>
+        <ContactSubBlockInfo>
+          <h2>DOJAZD</h2>
+          <p>Szkółka zlokalizowana jest na granicy Rumi oraz Dębogórze Wybudowanie. Istnieje możliwość dojazdu autobusem z dworca PKP Rumia nr 86. Szczegółowe informacje jak numer telefonu, adres można znaleźć na dole strony.
+          </p>
+          <StyledIconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+            onClick={scrollDown}
+          >
+            <StyledKeyboardArrowDownIcon
+              style={{ width: "100px", height: "100px" }}
+            />
+          </StyledIconButton>
+        </ContactSubBlockInfo>
+      </ContactBlockInfo>
+      <BlockWithMap>
+      <MapContainer center={[54.572573, 18.429189]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[54.572573, 18.429189]}>
+    <Popup>
+    <b>Magia Ogrodów. Beata Kulling-Nitkowska</b><br/>
+    Adres:<br/>
+    Dębogórska 8<br/>
+    84-230 Dębogórze Wybudowanie
+    </Popup>
+  </Marker>
+</MapContainer>
+      </BlockWithMap>
+    </div>
+  );
+};
+
+export default memo(Contact);
