bda0d2d1ec924d213579b887f9dbc200e4d106e0
Lukasz Nitkowski <lukasz.nitkowski@pl.pwc.com>
2021-01-09 17:24:30 +0100

    hashFix

--- magia-ogrodow/src/components/horizontalMenu/horizontalMenu.js
+++ magia-ogrodow/src/components/horizontalMenu/horizontalMenu.js
@@ -10,7 +10,7 @@ import logo from '../../images/logo.png'
 import {HorizontalMenuContainer,StyledBottomNavigation, StyledBottomNavigationAction} from './style.js'
 
 const HorizontalMenu = () => {
-  const pathname = window.location.pathname;
+  const pathname = window.location.hash;
   const [value, setValue] = useState(pathname);
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -24,12 +24,12 @@ const HorizontalMenu = () => {
         onChange={handleChange}
         showLabels
     >
-      <StyledBottomNavigationAction value='/' component={Link} to="/" label="O NAS" icon={<FaceIcon />} />
-      <StyledBottomNavigationAction value='/plantNursery' component={Link} to="/plantNursery" label="SZKÓŁKA ROŚLIN" icon={<HomeWorkIcon />} />
-      <StyledBottomNavigationAction value='/projects' component={Link} to="/projects" label="PROJEKTY" icon={<CreateIcon />} />
-      <StyledBottomNavigationAction value='/gardening' component={Link} to="/gardening" label="ZAKŁADANIE OGRODU" icon={<FilterVintageIcon />} />
-      <StyledBottomNavigationAction value='/gardenCare' component={Link} to="/gardenCare" label="PIELĘGNACJA" icon={<LocalFloristIcon />} />
-      <StyledBottomNavigationAction value='/contact' component={Link} to="/contact" label="KONTAKT" icon={<PhoneIcon />} />
+      <StyledBottomNavigationAction value='#/' component={Link} to="/" label="O NAS" icon={<FaceIcon />} />
+      <StyledBottomNavigationAction value='#/plantNursery' component={Link} to="/plantNursery" label="SZKÓŁKA ROŚLIN" icon={<HomeWorkIcon />} />
+      <StyledBottomNavigationAction value='#/projects' component={Link} to="/projects" label="PROJEKTY" icon={<CreateIcon />} />
+      <StyledBottomNavigationAction value='#/gardening' component={Link} to="/gardening" label="ZAKŁADANIE OGRODU" icon={<FilterVintageIcon />} />
+      <StyledBottomNavigationAction value='#/gardenCare' component={Link} to="/gardenCare" label="PIELĘGNACJA" icon={<LocalFloristIcon />} />
+      <StyledBottomNavigationAction value='#/contact' component={Link} to="/contact" label="KONTAKT" icon={<PhoneIcon />} />
     </StyledBottomNavigation>
     </HorizontalMenuContainer>
   );
