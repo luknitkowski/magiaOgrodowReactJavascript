f533d57608ead270c5b0844ef4b102229c66d7fd
Lukas <luknitkowski@gmail.com>
2021-04-27 21:36:51 +0200

    tsConfig

---
+++ magia-ogrodow/src/react-reveal.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-reveal/Fade';
+declare module 'react-reveal/LightSpeed';
+declare module 'react-reveal/Zoom';
+declare module 'react-reveal/Bounce';
+declare module 'react-reveal/Slide';
