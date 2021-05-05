0adb06a7262a807fc83b27efb50eee57438b9364
Lukas <luknitkowski@gmail.com>
2021-05-05 17:35:01 +0200

    enzymefix

--- magia-ogrodow/src/test/fullDialog.test.tsx
+++ magia-ogrodow/src/test/fullDialog.test.tsx
@@ -1,9 +1,12 @@
 import renderer from 'react-test-renderer'
+import { render, screen, fireEvent  } from '@testing-library/react'
 import EnzymeToJson from 'enzyme-to-json';
 import Enzyme,{ mount } from 'enzyme'; 
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import FullDialog from '../components/fullDialog';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const testProps = {
     dialogConfig: {
@@ -26,10 +29,6 @@ const testProps = {
 
 
 test('FullDialog renders correctly', () => {
-    Enzyme.configure({ adapter: new Adapter() });
     const fullDialog = mount(<FullDialog dialogConfig={testProps.dialogConfig} closeFullDialog={testProps.closeFullDialog} />);
     expect(EnzymeToJson(fullDialog)).toMatchSnapshot();
-  {/* const fullDialog = renderer.create(<FullDialog dialogConfig={testProps.dialogConfig} closeFullDialog={testProps.closeFullDialog} />).toJSON();
-  expect(fullDialog).toMatchSnapshot(); */}
 });
-
